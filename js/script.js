ba9c60990cad6425457956f2448e8b472beabb99
Adam <adamh93@verizon.net>
2017-02-09 20:59:29 -0500

    done: Creating a loop to add initial markers to the map; chore: Add bounce animation when the marker is clicked on, Create Knockout Array for recommendations list, Alter index.html for data binding for recommendations list

--- js/script.js
+++ js/script.js
@@ -1,6 +1,40 @@
+var initialMarkers = [
+    {
+    title: "Bubbly Tea",
+    lat: 40.7152875,
+    lng: -73.9977593
+    },
+    {
+    title: "Confucius Plaza",
+    lat: 40.7158642,
+    lng: -73.9954891
+    },
+    {
+    title: "Nom Wah Tea Palor",
+    lat: 40.7144448,
+    lng: -73.9982542
+    },
+    {
+    title: "Great NY Noodle Town",
+    lat: 40.7150319,
+    lng: -73.997038
+    },
+    {
+    title: "Golden Fung Wong Bakery Shop",
+    lat: 40.7151294,
+    lng: -73.9988711
+    },
+    {
+    title: "Noodle Village",
+    lat: 40.7141342,
+    lng: -73.9989576
+    }
 
+];
 
-var marker;
+var initialMarkers_length = initialMarkers.length;
+
+var markers = [];
 
 function initMap() {
   var uluru = {lat: 40.715272, lng: -73.9974404}; //Location on China Town
@@ -9,34 +43,38 @@ function initMap() {
     center: uluru
   });
 
-  marker = new google.maps.Marker({ //Bubbly Tea
-    position: {lat: 40.7152875, lng: -73.9977593},
-    animation: google.maps.Animation.DROP,
-    map: map
-  });
+  for (i = 0; i < initialMarkers_length; i++){
+    markers[i] = new google.maps.Marker({ //Bubbly Tea
+      position: {lat: initialMarkers[i].lat, lng: initialMarkers[i].lng},
+      animation: google.maps.Animation.DROP,
+      map: map
+    });
 
-  var marker2 = new google.maps.Marker({ //Confucius Plaza
-    position: {lat: 40.7158642, lng: -73.9954891},
-    map: map
-  });
-  var marker3 = new google.maps.Marker({ //Nom Wah Tea Palor
-    position: {lat: 40.7144448, lng: -73.9982542},
-    map: map
-  });
-  var marker4 = new google.maps.Marker({ //Great NY Noodle Town
-    position: {lat: 40.7150319, lng: -73.997038},
-    map: map
-  });
-  var marker5 = new google.maps.Marker({ //Golden Fung Wong Bakery Shop
-    position: {lat: 40.7151294, lng: -73.9988711},
-    map: map
-  });
+    markers[i].addListener('click', toggleBounce(markers[i]));
+  }
 
-  marker.addListener('click', toggleBounce);
+
+  // var marker2 = new google.maps.Marker({ //Confucius Plaza
+  //   position: {lat: 40.7158642, lng: -73.9954891},
+  //   map: map
+  // });
+  // var marker3 = new google.maps.Marker({ //Nom Wah Tea Palor
+  //   position: {lat: 40.7144448, lng: -73.9982542},
+  //   map: map
+  // });
+  // var marker4 = new google.maps.Marker({ //Great NY Noodle Town
+  //   position: {lat: 40.7150319, lng: -73.997038},
+  //   map: map
+  // });
+  // var marker5 = new google.maps.Marker({ //Golden Fung Wong Bakery Shop
+  //   position: {lat: 40.7151294, lng: -73.9988711},
+  //   map: map
+  // });
 
 }
 
-function toggleBounce() {
+//Toggle the bounce animation for selected marker
+function toggleBounce(marker) {
         if (marker.getAnimation() !== null) {
           marker.setAnimation(null);
         } else {
@@ -44,9 +82,36 @@ function toggleBounce() {
         }
       }
 
-google.maps.event.addDomListener(window, 'load', initialize);
-google.maps.event.addDomListener(window, "resize", function() {
-    var center = map.getCenter();
-    google.maps.event.trigger(map, "resize");
-    map.setCenter(center);
-  });
+// google.maps.event.addDomListener(window, 'load', initialize);
+// google.maps.event.addDomListener(window, "resize", function() {
+//     var center = map.getCenter();
+//     google.maps.event.trigger(map, "resize");
+//     map.setCenter(center);
+//   });
+
+
+
+  // var Location = function(data){
+  //   this.title = ko.observable(data.title);
+  //   this.lat = ko.observable(data.lat);
+  //   this.lng = ko.observable(data.lng);
+  //
+  // }
+
+
+
+
+  // var ViewModel = function(){
+  //   var self = this;    //--- referencing the ViewModel itself, thus the outer this outside of the div of where we applied the 'with data-bind'
+  //
+  //   //KnockOut array of list of recommended locations
+  //   this.locationsList = ko.observableArray([]);
+  //
+  //   //Add initial marker locations
+  //   initialMarkers.forEach(function(locationItem){
+  //     self.locationsList.push( new Location(locationItem) );
+  // });
+  //
+  // };
+  //
+  // ko.applyBindings(new ViewModel());
